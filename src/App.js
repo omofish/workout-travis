7a83e2f692a71381c0d6458de0560bd7d37473b6
omofish <jason_lzx@hotmail.com>
2020-03-10 22:07:38 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { Container } from 'semantic-ui-react';
-import SplashPage from './Pages/SplashPage';
-import WorkoutsPage from './Pages/WorkoutsPage';
-import TopMenu from './Components/TopMenu';
-import ProfilePage from './Pages/ProfilePage';
-import { WorkoutContext } from './context';
-import { firebase }from './firebaseDb';
-import 'firebase/database';
+import React, { useContext, useEffect, useState } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+import SplashPage from "./Pages/SplashPage";
+import WorkoutsPage from "./Pages/WorkoutsPage";
+import TopMenu from "./Components/TopMenu";
+import ProfilePage from "./Pages/ProfilePage";
+import { WorkoutContext } from "./context";
+import { firebase } from "./firebaseDb";
+import "firebase/database";
 import "firebase/auth";
 
 const App = () => {
@@ -16,38 +16,33 @@ const App = () => {
   useEffect(() => {
     firebase.auth().onAuthStateChanged(setUser);
   }, []);
-  
 
-  const workoutContext = useContext(WorkoutContext)
+  const workoutContext = useContext(WorkoutContext);
   const { equipment, workouts, countdown, muscleGroups } = workoutContext;
-  console.log('duration', countdown)
-  console.log('equip', equipment)
-  console.log('muscle groups', muscleGroups)
-  console.log('workout list', workouts)
-  console.log(user)
+  console.log("duration", countdown);
+  console.log("equip", equipment);
+  console.log("muscle groups", muscleGroups);
+  console.log("workout list", workouts);
+  console.log(user);
 
-  const withMenu = (page) => {
+  const withMenu = page => {
     return (
       <Container>
         <TopMenu user={user} />
         {page}
       </Container>
-    )
+    );
   };
 
   return (
     <BrowserRouter>
-      <Route 
-        exact 
-        path='/' 
-        render={() => withMenu(<SplashPage />)}
-      />
+      <Route exact path="/" render={() => withMenu(<SplashPage />)} />
       <Route
-        path='/workouts'
+        path="/workouts"
         render={() => withMenu(<WorkoutsPage user={user} />)}
       />
       <Route
-        path='/profile'
+        path="/profile"
         render={() => withMenu(<ProfilePage user={user} />)}
       />
     </BrowserRouter>
